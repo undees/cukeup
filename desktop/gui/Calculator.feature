61ccf89511650f9a7618adcf1af7b0f72742a3d2
Ian Dees <ian.s.dees@tek.com>
2013-09-15 20:00:51 -0700

    Add desktop example

---
+++ desktop/gui/Calculator.feature
@@ -0,0 +1,5 @@
+﻿Feature: Calculator
+
+Scenario: Add two numbers
+	When I multiply 2 and 3
+   	Then I should get 6
---
+++ desktop/gui/CalculatorSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.InputDevices;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+
+namespace Calc.Spec
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private Window window;
+
+        [Before]
+        public void Before()
+        {
+            Application application = Application.Launch("calc.exe");
+            window = application.GetWindow("Calculator", InitializeOption.NoCache);
+        }
+
+        [When(@"I multiply (.*) and (.*)")]
+        public void WhenIMultiply(string a, string b)
+        {
+            window.Keyboard.Enter(a + "*" + b + "=");
+        }
+        
+        [Then(@"I should get (.*)")]
+        public void ThenIShouldGet(string expected)
+        {
+            window.Get<Label>(expected);
+        }
+    }
+}
