ee3b2233741e8e6c7d4c2cd6aa3ff8eb4678fc9e
Ian Dees <undees@gmail.com>
2013-09-17 11:52:22 -0400

    Simpler pattern matching example

--- elixir/examples/ack.exs
+++ elixir/examples/ack.exs
@@ -1,6 +1,6 @@
 defmodule Ackermann do
-  def ack(m, n) when m == 0, do: n + 1
-  def ack(m, n) when n == 0, do: ack(m - 1, 1)
+  def ack(0, n), do: n + 1
+  def ack(m, 0), do: ack(m - 1, 1)
   def ack(m, n), do: ack(m - 1, ack(m, n - 1))
 end
 
